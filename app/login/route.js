e5d0cc323b649cf36b5e1509724fe9df7da12e8e
Rene kuhm <rdesigncastex@gmail.com>
2023-12-02 21:24:53 -0300

    empesando

---
+++ app/login/route.js
@@ -0,0 +1,6 @@
+import {NextResponse} from 'next/server'
+export const GET=(request)=>{
+    return NextResponse.json({
+        msg: "Hola Rene"
+    }) 
+}
---
+++ model/UserModel.models.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+export const UserModel = mongoose.models.user || mongoose.model("User", schema);
